58bdb394498747e66cca1a3fb8fb8e368f2185b0
junaidsalam639 <junaidsalam639@gmail.com>
2023-09-04 16:35:10 +0500

    'pro1'

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,39 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg sticky-top" data-aos="fade-down">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#"><img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width={40} /></a>
-                    <button class="navbar-toggler text-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                        <i class="fa-solid fa-bars-staggered"></i>
+            <nav className="navbar navbar-expand-lg sticky-top" data-aos="fade-down">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#"><img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width={40} /></a>
+                    <button className="navbar-toggler text-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                        <i className="fa-solid fa-bars-staggered"></i>
                     </button>
-                    <div class="collapse navbar-collapse text-center" id="navbarScroll">
-                        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
-                            <li class="nav-item fw-bold">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <div className="collapse navbar-collapse text-center" id="navbarScroll">
+                        <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+                            <li className="nav-item fw-bold">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
-                            <li class="nav-item fw-bold">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <li className="nav-item fw-bold">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
-                            <li class="nav-item fw-bold">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <li className="nav-item fw-bold">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
-                            <li class="nav-item fw-bold">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <li className="nav-item fw-bold">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
                         </ul>
-                        <form class="d-flex text-center" role="search" id="inner">
-                            <a href='../Signup.jsx'>login</a>
+                        <form className="d-flex text-center" role="search" id="inner">
+                            <Link to="Signup.jsx">Login</Link>
                         </form>
                     </div>
                 </div>
             </nav>
-
         </div>
     )
 }
 
-export default Navbar
+export default Navbar;
